361f18166f9551f9699aeb12169dce82bedde3c9
username <160256273+Vanshikaasingh09@users.noreply.github.com>
2025-04-27 10:36:13 +0530

    changes

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import streamlit as st
 
 # Read your HTML file
@@ -6,3 +7,13 @@ with open("index.html", "r") as file:
 
 # Display the HTML inside Streamlit
 st.components.v1.html(html_content, height=800, scrolling=True)
+=======
+import streamlit as st
+
+# Read your updated HTML file
+with open("index.html", "r", encoding='utf-8') as file:
+    html_content = file.read()
+
+# Display the HTML inside Streamlit
+st.components.v1.html(html_content, height=1000, scrolling=True)
+>>>>>>> 2d6bd53 (changes)
